5c0c8d8f25f6a13a50765505d29b08ae95224c7e
Alex4589 <64164865+Alex4589@users.noreply.github.com>
2021-09-16 14:47:48 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/*
+    Author: Alex Patino
+    Date: 9/13/2021
+    HW #1
+*/
+public class HelloWorld
+{
+        public static void main(String[] args)
+    {
+        System.out.println("Hello, World");
+    }
+}
